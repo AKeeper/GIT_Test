ed01d0e09f3b5a7d446eec99c0f01897eaeda280
AKeeper <akeeper83@gmail.com>
2017-12-16 22:40:55 +0200

    Added css gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 const gulp = require('gulp');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('html', function() {
-    gulp
-        .src('./src/index.html')
-        .pipe(gulp.dest('./build/'));
+gulp
+    .src('./src/index.html')
+    .pipe(gulp.dest('./build/'));
 });
 
-gulp.task('default', ['html']);
+gulp.task('css', function() {
+gulp
+    .src([
+        './src/styles/main.less',
+        './src/styles/**/*.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css'
+    ])
+    .pipe(less())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+    }))
+    .pipe(concat('build.css'))
+    .pipe(cleanCSS({compatibility: 'ie9'}))
+    .pipe(gulp.dest('./build/scc/'));
+});
+gulp.task('default', ['html', 'css']);
