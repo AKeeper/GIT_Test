702bbc84764490801226344248704684d10b71e0
AKeeper <akeeper83@gmail.com>
2017-12-16 23:37:06 +0200

    Added browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,21 @@ const less = require('gulp-less');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
 
 gulp.task('html', function() {
 gulp
     .src('./src/index.html')
-    .pipe(gulp.dest('./build/'));
+    .pipe(gulp.dest('./build/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('css', function() {
 gulp
     .src([
         './src/styles/main.less',
-        './src/styles/**/*.css',
-        './node_modules/bootstrap/dist/css/bootstrap.css'
+        './src/styles/**/*.css'
+        // './node_modules/bootstrap/dist/css/bootstrap.css'
     ])
     .pipe(less())
     .pipe(autoprefixer({
@@ -23,6 +25,20 @@ gulp
     }))
     .pipe(concat('build.css'))
     .pipe(cleanCSS({compatibility: 'ie9'}))
-    .pipe(gulp.dest('./build/scc/'));
+    .pipe(gulp.dest('./build/css/'))
+    .pipe(browserSync.stream());
 });
-gulp.task('default', ['html', 'css']);
+
+gulp.task('serve', ['html', 'css'], function(){
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+});
+
+gulp.task('watch', ['serve'], function () {
+    gulp.watch(['./src/styles/**/*.css', './src/styles/**/*.less'], ['css']);
+    gulp.watch('./src/index.html', ['html']);
+});
+gulp.task('default', ['watch']);
